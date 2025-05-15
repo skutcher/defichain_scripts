b2c42f6a0129f5c27a8df6acb9449d774dbbc1a3
smo <skutcher3@gmail.com>
2025-05-15 17:09:10 +0200

    move files into new project structure, do some changes to use dotenv and start prepare using functions

---
+++ defichainScripts/stats/burn_fee_ni.py
@@ -0,0 +1,60 @@
+import os
+from defichain import Node
+from dotenv import load_dotenv
+
+def calc():
+    username=os.getenv('USERNAME')
+    password=os.getenv('PASSWORD')
+    node =  Node(username, password, url="127.0.0.1", port=8554)
+
+
+
+    #Variables
+    endBlock=5015800
+    depth=30*2880
+    limit=8000
+
+    sumAll=0
+    sumBBB=0
+
+    #complete burn of DUSD with specified borders
+    allBurnList=node.accounts.listburnhistory(maxBlockHeight=endBlock, token="DUSD",depth=depth, txtype="i", limit=100000)
+
+    #Cycle for overall DUSD burn amount
+    for x in allBurnList:
+        for amount in x["amounts"]:
+            if amount.endswith("DUSD"):
+                amount=amount.replace("@DUSD","")
+                sumAll += float(amount)
+
+    #list of BBB tx with specified borders
+    BBBtxList=node.accounts.listaccounthistory(owner="df1q0ulwgygkg0lwk5aaqfkmkx7jrvf4zymj0yyfef", maxBlockHeight=endBlock, no_rewards=True, depth=depth, limit=limit)
+    TXcount=(len(BBBtxList))
+
+    #Cycle for BBB DUSD burn amount
+    for x in BBBtxList:
+        burnList=node.accounts.getaccounthistory("8defichainBurnAddressXXXXXXXdRQkSm",x["blockHeight"],x["txn"])
+        for amount in burnList["amounts"]:
+            if amount.endswith("DUSD"):
+                amount=amount.replace("@DUSD","")
+                sumBBB += float(amount)
+
+    #Output data:
+    print("BBB stats")
+
+    print("End block is: ",endBlock)
+
+    print("30 days period in Blocks: ",depth)
+
+    print("Number of BBB transactions: ",TXcount)
+
+    print("Overall burn amount: ",sumAll)
+    print("BBB burn amount: ",sumBBB)
+    print("Burn by Dex Fee: ",(sumAll-sumBBB))
+
+def main():
+    load_dotenv()
+    calc()
+
+if __name__ == "__main__":
+    main()
---
+++ defichainScripts/stats/dTokenNumbersOnDVM.py
@@ -0,0 +1,98 @@
+import os
+import sys
+import json
+import time
+from defichain import Node
+from dotenv import load_dotenv
+
+def calc(blockHeight):
+    username=os.getenv('USERNAME')
+    password=os.getenv('PASSWORD')
+    node =  Node(username, password, url="127.0.0.1", port=8554)
+
+    while node.blockchain.getblockcount() < blockHeight:
+        print('sleep 3s')
+        time.sleep(3)
+
+    #List of Poolpairs with DUSD
+    LMpoolsList=node.poolpair.listpoolpairs(start=367,limit=500)
+
+    #List of Oracle prices
+    oraclePrices=node.oracles.listprices(limit=1000)
+
+    #Variables for output
+    freeAssets=0
+    backedAssets=0
+    algoAssets=0
+    output={}
+    currentBlock=node.blockchain.getblockcount()
+
+    for value in LMpoolsList.values(): # Cycle over the list of poolpairs
+        if value["symbol"].endswith("DUSD") and not value["symbol"].startswith("USD") and not value["symbol"].startswith("EUR") and not value["symbol"].startswith("BURN"): # Only the pools with DUSD without the Stables and BURN
+            id = value["idTokenA"]
+
+            #Getting the numbers for tokens
+            token=node.tokens.gettoken(id)
+            output[id] = {}
+            output[id]["symbol"] = token[id]["symbolKey"]
+
+            #Getting the prices for tokens
+            oracle = next(filter(lambda oracle: oracle['token'] == token[id]["symbolKey"], oraclePrices), None)
+            if oracle is not None:
+                try:
+                    oracle_price = node.oracles.getprice(token[id]["symbolKey"],"USD")
+                    output[id]["price"] = oracle_price
+
+                    #Getting amounts measured in DUSD
+                    output[id]["dusdAmount"] = token["freeOnDVM"]*output[id]["price"]
+                    output[id]["loanAmount"] = token["openLoans"]*output[id]["price"]
+                    freeAssets+=output[id]["dusdAmount"]
+                    backedAssets+=output[id]["loanAmount"]
+                except :
+                    print("no oracle data")
+
+    #Algo dAssets
+    algoAssets=freeAssets-backedAssets
+
+    #Numbers for DUSD
+    dusdNumbers=node.tokens.gettoken("DUSD")
+    freeDUSD=dusdNumbers["freeOnDVM"]
+    backedDUSD=dusdNumbers["openLoans"]
+    algoDUSD=freeDUSD-backedDUSD
+
+    #AlgoRatios
+    algoRatio=round((algoAssets+algoDUSD)/(freeAssets+freeDUSD)*100,2)
+    algoRatioDUSD=round((algoDUSD)/(freeDUSD)*100,2)
+
+    #Output
+    #print (json.dumps(output, indent=2, default=str))
+
+    print("Current block: ",currentBlock)
+    print("")
+    print("FREE   dAssets on DVM: ", "{:,}".format(int(freeAssets)).replace(","," "), "measured in DUSD (oracle price used)")
+    print("BACKED dAssets on DVM: ", "{:,}".format(int(backedAssets)).replace(","," "), "measured in DUSD (oracle price used)")
+    print("ALGO   dAssets on DVM: ", "{:,}".format(int(algoAssets)).replace(","," "), "measured in DUSD (oracle price used)")
+    print("")
+    print("FREE   DUSD on DVM: ", "{:,}".format(int(freeDUSD)).replace(","," "), "measured in DUSD (oracle price used)")
+    print("BACKED DUSD on DVM: ", "{:,}".format(int(backedDUSD)).replace(","," "), "measured in DUSD (oracle price used)")
+    print("ALGO   DUSD on DVM: ", "{:,}".format(int(algoDUSD)).replace(","," "), "measured in DUSD (oracle price used)")
+    print("")
+    print("DUSD ALGO ratio: ",algoRatioDUSD, " %")
+    print("")
+    print("Total amount of FREE   DUSD & dAssets on DVM: ", "{:,}".format(int(freeAssets+freeDUSD)).replace(","," "), "measured in DUSD (oracle price used)")
+    print("Total amount of BACKED DUSD & dAssets on DVM: ", "{:,}".format(int(backedAssets+backedDUSD)).replace(","," "), "measured in DUSD (oracle price used)")
+    print("Total amount of ALGO   DUSD & dAssets on DVM: ", "{:,}".format(int(algoAssets+algoDUSD)).replace(","," "), "measured in DUSD (oracle price used)")
+    print("")
+    print("Overall ALGO ratio: ",algoRatio, " %")
+    print("")
+
+
+def main():
+    load_dotenv()
+    # TODO: check that modified node is running
+    # otherwise dusdNumbers["freeOnDVM"] throws an error later
+    print("ENSURE MODIFIED NODE IS RUNNING")
+    calc(int(sys.argv[1]))
+
+if __name__ == "__main__":
+    main()
